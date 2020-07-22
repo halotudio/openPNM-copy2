24a2ec7d98e4a157e1e70155044d691337e1419b
Mehrez Agnaou <magnaoulab@gmail.com>
2020-07-22 14:08:17 -0400

    added example script to export into comsol

---
+++ scripts/example_export_to_comsol.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(1, '/home/mehrez/Desktop/repositories/OpenPNM/')
+import openpnm as op
+import numpy as np
+
+# work space and project
+ws = op.Workspace()
+proj = ws.new_project()
+
+# network
+np.random.seed(7)
+net = op.network.Cubic(shape=[3, 3, 1], spacing=1e-4, project=proj)
+
+# geometry
+geo = op.geometry.StickAndBall(network=net,
+                               pores=net.Ps,
+                               throats=net.Ts)
+
+# phase
+phase = op.phases.Water(network=net)
+
+# output results
+# proj.export_data(phases=[phase], filename='OUT', filetype='XDMF')
+proj.export_data(filename='comsol_test', filetype='Comsol')
